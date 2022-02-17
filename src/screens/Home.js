29b37f5c94599702908effa2f02f17d61c43f82d
ahmadsoran <ahmadsorann@gmail.com>
2022-02-17 23:56:18 +0300

    add realtime data

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, ImageBackground } from "react-native";
 import {
   Layout,
@@ -10,16 +10,77 @@ import {
 } from "react-native-rapi-ui";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HomeText from "../components/homeText";
+import adhan from 'adhan'
+import moment from 'moment';
+import * as Location from 'expo-location';
 
 export default function ({ navigation }) {
   const { isDarkmode, setTheme } = useTheme();
+  const [location, setLocation] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('may Permission to access location is  denied or no there is no internet connection');
+        return;
+      }
+
+
+      const locations = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = locations.coords
+      setLocation([latitude, longitude]);
+    })();
+  }, ['one']);
+
+  var date = new Date(Date.now());
+  var coordinates = new adhan.Coordinates(...location);
+  var params = adhan.CalculationMethod.MuslimWorldLeague()
+  params.madhab = adhan.Madhab.Hanafi;
+  var prayerTimes = new adhan.PrayerTimes(coordinates, date, params);
+  const fajr = moment(prayerTimes.fajr).format("h:mm");
+  const dhuhr = moment(prayerTimes.dhuhr).format("h:mm");
+  const asr = moment(prayerTimes.asr).format("h:mm");
+  const maghrib = moment(prayerTimes.maghrib).format("h:mm");
+  const isha = moment(prayerTimes.isha).format("h:mm");
+  var current = prayerTimes.currentPrayer();
+  var nextPray = prayerTimes.nextPrayer();
+
+
   return (
 
     <Layout>
       <ImageBackground source={{ uri: 'https://muslimmatters.org/wp-content/uploads/shutterstock_168576587.jpg' }}
         style={styles.image} resizeMode="cover" blurRadius={3}
       />
+      <View style={{
+        // zIndex: -1,
+        position: 'absolute',
+        backgroundColor: isDarkmode ? '#02020298' : '#ffffff00',
+        height: '200%',
+        width: '100%',
+        top: 0,
 
+      }} />
+      {errorMsg &&
+        <View style={{
+          zIndex: 10,
+          position: 'absolute',
+          backgroundColor: 'white',
+          height: '120%',
+          padding: 20,
+          width: '100%',
+          top: 0,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+
+
+        }} >
+          <Text style={{ color: 'red', fontSize: 40 }}>{errorMsg}</Text>
+        </View>
+      }
       <SafeAreaView />
 
       <View
@@ -28,6 +89,7 @@ export default function ({ navigation }) {
           minHeight: '100%',
         }}
       >
+
 
         <View style={{ position: 'relative', width: '30%' }}>
 
@@ -38,11 +100,11 @@ export default function ({ navigation }) {
         </View>
         <Section backgroundColor={isDarkmode ? '#02020225' : '#ffffff2d'} style={{ marginTop: 50 }} >
           <SectionContent>
-            <HomeText time={'5:00'} nameTime={'بەیانیان'} />
-            <HomeText time={'5:00'} nameTime={'بەیانیان'} />
-            <HomeText time={'5:00'} nameTime={'بەیانیان'} />
-            <HomeText time={'5:00'} nameTime={'بەیانیان'} />
-            <HomeText time={'5:00'} nameTime={'بەیانیان'} />
+            <HomeText time={fajr} nameTime={'بەیانیان'} />
+            <HomeText time={dhuhr} nameTime={'نیوەڕۆ'} />
+            <HomeText time={asr} nameTime={'عەسر'} />
+            <HomeText time={maghrib} nameTime={'ێوارە'} />
+            <HomeText time={isha} nameTime={'عيشا'} />
           </SectionContent>
         </Section>
       </View>
