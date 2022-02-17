a86980d6ec266ed0e71874799f1b1a298f718ef7
ahmadsoran <ahmadsorann@gmail.com>
2022-02-17 23:55:16 +0300

    .

--- src/components/homeText.js
+++ src/components/homeText.js
@@ -1,18 +1,18 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { themeColor, useTheme } from 'react-native-rapi-ui';
+import { View, Text, Platform } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { useTheme } from 'react-native-rapi-ui';
 
 const HomeText = (props, { navigation }) => {
     const { isDarkmode } = useTheme();
-    console.log(isDarkmode);
+
     return (
         <View style={{
             backgroundColor: isDarkmode ? '#02020286' : '#ffffff80', padding: 10,
             borderRadius: 5, marginVertical: 5, display: 'flex', justifyContent: 'space-between'
             , flexDirection: 'row'
         }}>
-            <Text style={{ textAlign: 'right', color: !isDarkmode ? '#02020286' : '#ffffff80', fontSize: 25 }}>{props.time}</Text>
-            <Text style={{ textAlign: 'right', color: !isDarkmode ? '#02020286' : '#ffffff80', fontSize: 25 }}>{props.nameTime}</Text>
+            <Text style={{ textAlign: 'right', color: !isDarkmode ? '#02020286' : '#fffffffb', fontSize: 25 }}>{props.time}</Text>
+            <Text style={{ textAlign: 'right', color: !isDarkmode ? '#02020286' : '#fffffff8', fontSize: 25 }}>{props.nameTime}</Text>
         </View>
     );
 };
