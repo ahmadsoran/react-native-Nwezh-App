19883809f24512169c8d675978a0d2faf0593f6b
ahmadsoran <ahmadsorann@gmail.com>
2022-02-18 23:15:13 +0300

    add store conf from redux

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { ThemeProvider } from "react-native-rapi-ui";
+import { Provider } from 'react-redux'
+import { store } from "./src/services/store";
+
 export default function App() {
   return (
-    <ThemeProvider>
-      <AppNavigator />
-    </ThemeProvider>
+    <Provider store={store}>
+
+      <ThemeProvider>
+        <AppNavigator />
+      </ThemeProvider>
+    </Provider>
   );
 }
